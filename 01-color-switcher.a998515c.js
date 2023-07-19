1542ddacb7e878f98ec43d33a811bcde46356089
TetianaHutsal <125972430+TetianaHutsal@users.noreply.github.com>
2023-07-19 17:25:29 +0000

    Deploying to gh-pages from @ TetianaHutsal/goit-js-hw-09@895f0aeac2a3d3ef2a86427c407a6f4fd0ab4cca 🚀

---
+++ 01-color-switcher.a998515c.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");document.body;t.addEventListener("click",onStartBtnClick),e.addEventListener("click",onStopBtnClick)}();
+//# sourceMappingURL=01-color-switcher.a998515c.js.map
